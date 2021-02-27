a6086675767f2f232f45bcc83bb46db42cfd1d62
pranavganore <pranavganore@10.0.0.14>
2021-02-27 12:02:29 -0500

    merge sort and insertion sort

--- Algorithms CodeBase/src/com/pbg/SORTING/Insertion_Sort.java
+++ Algorithms CodeBase/src/com/pbg/SORTING/Insertion_Sort.java
@@ -1,5 +1,7 @@
 package com.pbg.SORTING;
 
+import java.util.Arrays;
+
 /* --------- 3.	Insertion Sort  --------- */
 
 //	A Decrease & Conquer Strategy	(A problem is divided into single sub-problem)
@@ -20,8 +22,122 @@ public class Insertion_Sort {
 	//	Recursive way
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//	Define an input array
+		Integer[] input_Array = {7,2,5,3,9,6,8,1,4,10};
+//		Integer[] input_Array = {10,9,8,7,6,5,4,3,2,1};
+		Integer[] sorted_Array = null;
+		
+		//	Sort it using Selection Sort Algorithm - Iterative Way
+		sorted_Array = insertion_Sort_iterative(input_Array);
+				
+		//	Display Results
+		System.out.println("\n\nThe final sorted array using Insertion Sort "
+				+ "(Iterative Approach) is : " + Arrays.toString(sorted_Array));
+		
+		//	Sort it using Selection Sort Algorithm - Recursive Way
+		sorted_Array = insertion_Sort_recursive(input_Array, input_Array.length);
+		
+		//	Display Results
+		System.out.println("\n\nThe final sorted array using Insertion Sort "
+				+ "(Recursive Approach) is : " + Arrays.toString(sorted_Array) + "\n\n");
+		
+	}
 
+	private static Integer[] insertion_Sort_iterative(Integer[] int_Array) {
+		if (int_Array.length <= 1) return int_Array;	// Base case - Check if array has single element or empty
+
+		for (int i = 2 ; i < int_Array.length ; i++) {	// loop over from 2 to n
+			// Now initialize & insert the ith(last) element into its rightful place
+			int last = int_Array[i];
+			int j = i - 1;
+			
+			while ((j >= 1) && (int_Array[j] > last)) {	//	Shift all elements to right
+				int_Array[j+1] = int_Array[j];			//	until you find nth elements
+				j--;									//	rightful place
+				System.out.println("Right Shift !");	// Diagnostic Print
+			}// Now an empty space is created where the nth element will go
+			
+			int_Array[j+1] = last;
+			// Inserting the nth(last) element into its rightful place
+//			Diagnostic Prints
+			System.out.println("Element : " + last + " inserted at -> [" + (j+1) + "]");
+			System.out.println("Array : " + Arrays.toString(int_Array));
+		}
+		return int_Array;
+	}
+
+	private static Integer[] insertion_Sort_recursive(Integer[] int_Array, int array_size) {
+		if (array_size <= 1) return int_Array;	// Base case - Check if array has single element or empty
+		insertion_Sort_recursive(int_Array, (array_size-1));	//	Recursion [break the problem down]
+		
+		// Now initialize & insert the nth(last) element into its rightful place
+		int nth = int_Array[array_size-1];
+		int j = array_size - 2 ;
+		
+		while((j >= 0) && (int_Array[j] > nth)){	//	Shift all elements to right 
+			int_Array[j+1] = int_Array[j];			//	until you find nth elements 
+			j = j - 1;								//	rightful place
+			System.out.println("Right Shift !");
+		}	// Now an empty space is created where the nth element will go
+		
+		int_Array[j+1] = nth;	// Inserting the nth(last) element into its rightful place
+//		Diagnostic Prints
+		System.out.println("Element : " + nth + " inserted at -> [" + (j+1) + "]");
+		System.out.println("Array : " + Arrays.toString(int_Array));
+		
+		return int_Array;
 	}
 
 }
+
+/**
+ * 		Design Strategy 2 : Decrease And Conquer	(1 sub-problem)
+ * 
+ * 	-	Decrease the problem of size n to size (n-1).
+ * 	-	Assume you have solved the problem of size (n-1).
+ * 	-	Solve the problem of size n using the solution to the problem of size (n-1)
+ * 	-	RECURSIVE (if TOP-DOWN) and ITERATIVE (if BOTTOM-UP)
+ * 
+ */
+
+/*	Pseudocode :: 
+ * 
+ *	Recursive Version (Top-Down Approach) :	
+ *	
+ *	def insertionSort(A,n) : 	// array A[1...n]
+ *		if(n <= 1) return;
+ *		insertionSort(A,n-1);
+ *		// Now insert the nth element into its place
+ *		j = n-1
+ *		while j >= 1 and A[j] > A[j+1] : 
+ *			swap(A[j+1],A[j])
+ *			j = j - 1
+ *		return
+ *
+ *	++ Slightly better / optimized approach (instead of bubble swaps use shifting)
+ *	def insertionSort(A,n) : 	// array A[1...n]
+ *		if(n <= 1) return;
+ *		insertionSort(A,n-1);
+ *		// Now insert the nth element into its place
+ *		nth = A[n-1];
+ *		j = n-2
+ *		while j >= 1 and A[j] > nth : 
+ *			A[j+1] = A[j]		//	Shift operation (to right) [slightly more efficient]
+ *			j = j - 1
+ *		A[j+1] = nth			//	insert the nth element to its rightful place in array
+ *		return
+ *
+ *	
+ *	Iterative Version (Bottom-Up approach) :
+ *
+ *	def insertionSort(A) : 	// array A[1...n]
+ *		if(n <= 1) return;
+ *		for i = 2 to n :
+ *			ith = A[i]
+ *			j = i - 1
+ *			while j >= 1 AND A[j] > ith :
+ *				A[j+1] = A[j]
+ *				j = j - 1
+ *			A[j+1] = ith
+ *		return
+ */
--- Algorithms CodeBase/src/com/pbg/SORTING/Merge_Sort.java
+++ Algorithms CodeBase/src/com/pbg/SORTING/Merge_Sort.java
@@ -1,5 +1,7 @@
 package com.pbg.SORTING;
 
+import java.util.Arrays;
+
 /* --------- 4.	Merge Sort --------- */
 
 //	A Divide & Conquer Strategy	(A problem is divided into multiple sub-problems)
@@ -14,9 +16,106 @@ package com.pbg.SORTING;
 public class Merge_Sort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        int arr[] = { 12, 8, 11, 13, 5, 2, 6, 10, 4, 7, 1, 3, 9 };
+        
+        System.out.println("Given Array : " + Arrays.toString(arr));
+
+        Merge_Sort obj = new Merge_Sort();
+        obj.merge_sort(arr, 0, arr.length - 1);
+        
+		//	Display Results
+		System.out.println("\n\nThe final sorted array using MERGE Sort "
+				+ "(Recursive Approach) is : " + Arrays.toString(arr) + "\n\n");
+		
 
 	}
+	
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void merge_sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Find the middle point
+            int m =l+ (r-l)/2;
+ 
+            // Sort first and second halves
+            merge_sort(arr, l, m);
+            merge_sort(arr, m + 1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+	// Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+ 
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+        
+        //Diagnostic Prints
+        System.out.println("\nIteraton :");
+        System.out.println("L = " + Arrays.toString(L) + 
+        					"\tR = " + Arrays.toString(R));
+        
+        /* Merge the temp arrays */
+ 
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+ 
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+        
+        //Diagnostic Prints
+        System.out.println("Merge : L+R = " + Arrays.toString(arr));
+        slowdown_forDiagnostics();
+    }
+ 
+	private static void slowdown_forDiagnostics() {
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+ 
 
 }
 
@@ -26,7 +125,55 @@ public class Merge_Sort {
  */
 
 /*
- * 	Pseudocode - Merge Sort
+ * 	Pseudocode - MERGE SORT
+ *
+ *	MERGESORT(A):
+ *		n = length(A)
+ *		if n<=1:return A	// Base case : If A has length 1 its obvio already sorted
+ *
+ *	// First recursively breakup the array all the way down to the base cases
+ *		L = MERGESORT(A[ 1 : n/2])		//	Sort the left half first
+ *		R = MERGESORT(A[ n/2+1 : n])	//	Then sort the right half
+ *		return MERGE(L,R)			//	Merge the two halves 
+ *
+ *
+ *	++ Internal implementation using helper method
+ *	function MERGESORT(A):
+ *		mergeSortHelper(A,0,length(A)-1)
+ *
+ *	function mergeSortHelper(A, start_index, end_index):
+ *		if start >= end return
+ *		mid = (start + end)/2
+ *		mergeSortHelper(A, start, mid)
+ *		mergeSortHelper(A, mid+1, end)
+ *		i = start
+ *		j = mid + 1
+ *		aux = an empty array of size [end - start + 1]
+ *		while i<= mid AND j<= end:
+ *			if A[i] <= A[j]
+ *				aux.append(A[i])
+ *				i++
+ *			else	//i.e. if A[i]>A[j]
+ *				aux.append(A[j])
+ *				j++
+ *		while i <= mid :
+ *			aux.append(A[i])
+ *			i++
+ *		while j <= end:
+ *			aux.append(A[j])
+ *			j++
+ *		A[start ... end] = aux
+ *		return
+ */
+
+/**
+ * 		Design Strategy 3 : Divide And Conquer	(Multiple sub-problems [>1])
+ * 
+ * 	-	'DIVIDE' the problem into several instances of the problem (most often two),
+ * 		generally of same size
+ * 	-	'SOLVE' the smaller instances (typically using recursion)
+ * 	-	'COMBINE' the solutions to the smaller instances to get the solution to the 
+ * 		original problem
  * 
  */
 
